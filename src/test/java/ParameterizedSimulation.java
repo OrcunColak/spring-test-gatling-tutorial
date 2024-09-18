2b5ae20aa6c0b36840ffedd8215b64ab40ccac53
Orçun Çolak <orcun.colak@hazelcast.com>
2024-09-18 09:21:51 +0300

    Move packages to test root

--- src/test/java/ParameterizedSimulation.java
+++ src/test/java/ParameterizedSimulation.java
@@ -1,5 +1,3 @@
-package com.colak.springtutorial.controller;
-
 import io.gatling.javaapi.core.CoreDsl;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
--- src/test/java/PeakTrafficSimulation.java
+++ src/test/java/PeakTrafficSimulation.java
@@ -1,25 +1,26 @@
-package com.colak.springtutorial.controller;
-
 import io.gatling.javaapi.core.CoreDsl;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpDsl;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
 
-public class RampUpSimulation extends Simulation {
+public class PeakTrafficSimulation extends Simulation {
 
     HttpProtocolBuilder httpProtocol = HttpDsl.http
             .baseUrl("http://localhost:8080")
             .acceptHeader("application/json");
 
-    ScenarioBuilder scn = CoreDsl.scenario("Ramp-Up Simulation")
+    ScenarioBuilder scn = CoreDsl.scenario("Peak Traffic Simulation")
             .exec(HttpDsl.http("request_hello")
                     .get("/api/hello")
                     .check(HttpDsl.status().is(200)));
 
     {
         setUp(
-                scn.injectOpen(CoreDsl.rampUsers(1000).during(600)) // Ramp up to 1000 users over 10 minutes
+                scn.injectOpen(
+                        CoreDsl.constantUsersPerSec(100).during(300), // Constant load of 100 users per second
+                        CoreDsl.rampUsersPerSec(10).to(100).during(600) // Gradually increase from 10 to 100 users per second
+                )
         ).protocols(httpProtocol);
     }
 }
